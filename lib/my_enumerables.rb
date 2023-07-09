cf34cd2a1de7eea2d73dc1d7070b1111d5a038d0
Adzi <belkoadam@gmail.com>
2023-07-09 18:10:58 +1000

    All solved

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,68 @@
 module Enumerable
-  # Your code goes here
+  def my_all?
+    my_each { |item| return false unless yield(item) }
+    true
+  end
+
+  def my_any?
+    my_each { |item| return true if yield(item) }
+    false
+  end
+
+  def my_count
+    count = 0
+    if block_given?
+      my_each { |item| count += 1 if yield(item) }
+    else
+      count = size
+    end
+    count
+  end
+
+  def my_each_with_index
+    index = 0
+    my_each do |item|
+      yield(item, index)
+      index += 1
+    end
+  end
+
+  def my_inject(initial = nil)
+    return unless block_given?
+
+    arr = to_a
+    accumulator = initial || arr.shift
+
+    arr.my_each do |item|
+      accumulator = yield(accumulator, item)
+    end
+
+    accumulator
+  end
+
+  def my_map
+    new_array = []
+
+    my_each do |item|
+      current = yield(item)
+      new_array << current
+    end
+
+    new_array
+  end
+
+  def my_none?
+    my_each { |item| return false if yield(item) }
+    true
+  end
+
+  def my_select
+    new_array = []
+
+    my_each { |item| new_array << item if yield(item) }
+
+    new_array
+  end
 end
 
 # You will first have to define my_each
@@ -7,5 +70,9 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    for item in self
+      yield(item)
+    end
+  end
 end
